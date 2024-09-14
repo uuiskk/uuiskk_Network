a2777f29dd0b4cf9316edbe8d87c5d08fe4eeb62
nhn-academy-marco <marco@ nhnacademy.com>
2024-09-14 20:08:55 +0900

    step5 - exercise

--- src/main/java/com/nhnacademy/server/method/response/impl/PortResponse.java
+++ src/main/java/com/nhnacademy/server/method/response/impl/PortResponse.java
@@ -19,7 +19,34 @@ public class PortResponse implements Response {
 
     @Override
     public String execute(String value) {
-        //TODO#1 OS로부터 오픈되어 있는 Prot를 조회후 반환 합니다.
+        /* TODO#1 OS로부터 오픈되어 있는 Prot를 조회후 반환 합니다.
+         - value(port) 에 값이 존재 하지 않는다면 열려있는 모든 port를 반환 합니다.
+         - value(port) 값이 존재 한다면 해당 port에 해당되는 값을 반환 합니다.
+         - 다음과 같은 형식으로 반환 됩니다.
+        TCP *:49742
+        TCP *:49742
+        TCP *:7000
+        TCP *:7000
+        TCP *:5000
+        TCP *:5000
+        TCP *:7797
+        TCP *:7797
+        TCP *:55920
+        TCP 127.0.0.1:16105
+        TCP 127.0.0.1:16115
+        TCP 127.0.0.1:16107
+        TCP 127.0.0.1:16117
+        TCP *:19875
+        TCP 127.0.0.1:19876
+        TCP 127.0.0.1:63342
+        TCP 127.0.0.1:52304
+        TCP *:8888
+        TCP 127.0.0.1:64120
+        TCP 127.0.0.1:3376
+        TCP 127.0.0.1:62451
+        TCP 127.0.0.1:64913
+        */
+
         StringBuilder sb = new StringBuilder();
 
         return sb.toString();
