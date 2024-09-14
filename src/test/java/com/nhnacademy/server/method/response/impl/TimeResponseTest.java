db709ebbb1b0eeffeace4fb00dc39ef6a4291b77
nhn-academy-marco <marco@ nhnacademy.com>
2024-09-14 17:35:10 +0900

    exercise-step3

--- src/test/java/com/nhnacademy/server/method/response/impl/TimeResponseTest.java
+++ src/test/java/com/nhnacademy/server/method/response/impl/TimeResponseTest.java
@@ -23,13 +23,13 @@ class TimeResponseTest {
     @Test
     void getMethod() {
         //TODO#5 getMethod()를 호출 후 time과 일치하는지 검증하세요
-        Assertions.assertEquals("time",response.getMethod());
+
     }
 
     @Test
     void validate(){
         //TODO#6 response.validate()를 검증하세요
-        Assertions.assertTrue(response.validate("time"));
+
     }
 
     @Test
@@ -46,21 +46,13 @@ class TimeResponseTest {
     @DisplayName("pattern : yyyy-MM-dd")
     void execute2() {
         //TODO#7 execute1() 테스트를 기반으로 "yyyy-MM-dd" 검증하는 코드를 작성하세요
-        String pattern="yyyy-MM-dd";
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(pattern);
-        String actual = LocalDateTime.now().format(dateTimeFormatter);
-        log.debug("pattern:{},actual:{}",pattern,actual);
-        Assertions.assertEquals(actual, response.execute(pattern));
+
     }
 
     @Test
     @DisplayName("pattern : yyyy-MM-dd HH:mm")
     void execute3() {
-        //TODO#7 execute1() 테스트를 기반으로 "yyyy-MM-dd HH:mm" 검증하는 코드를 작성하세요.
-        String pattern="yyyy-MM-dd HH:mm";
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(pattern);
-        String actual = LocalDateTime.now().format(dateTimeFormatter);
-        log.debug("pattern:{},actual:{}",pattern,actual);
-        Assertions.assertEquals(actual, response.execute(pattern));
+        //TODO#8 execute1() 테스트를 기반으로 "yyyy-MM-dd HH:mm" 검증하는 코드를 작성하세요.
+
     }
 }
