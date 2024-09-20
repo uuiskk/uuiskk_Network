a6394cffe69e73982ea2a87e78319e9e03925ce4
nhn-academy-marco <marco@ nhnacademy.com>
2024-09-20 17:01:57 +0900

    exercise-step9 추가

--- src/main/java/com/nhnacademy/server/method/response/ResponseFactory.java
+++ src/main/java/com/nhnacademy/server/method/response/ResponseFactory.java
@@ -37,17 +37,17 @@ public class ResponseFactory {
     
     static {
         //TODO#2-1 "com.nhnacademy.server" 페키지를 기준으로 class를 scan 합니다.
-        Reflections reflections = new Reflections("com.nhnacademy.server");
+        Reflections reflections = new Reflections("do something");
         //TODO#2-2 reflections로 부터 Response.class를 구현한 subType을 조회 합니다.
-        Set<Class<? extends Response>> classes = reflections.getSubTypesOf(Response.class);
+        Set<Class<? extends Response>> classes = null;
 
         for (Class<? extends Response> clazz : classes) {
             try {
                 //TODO#2-3 getDeclaredConstructor().newInstance() method를 호출해서 인스턴스를 생성 합니다.
-                Response response = clazz.getDeclaredConstructor().newInstance();
+                Response response = null;
                 log.debug("response-factory init :  instance :{}", response.getClass().getName() );
                 //TODO#2-4 생성된 response instance를 responseList에 추가 합니다.
-                responseList.add(response);
+
             } catch (Exception e) {
                 e.printStackTrace();
             }
