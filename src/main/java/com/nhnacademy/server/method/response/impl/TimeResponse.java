d7c1f3bf519b0e59b71a4d1dcbcf9c10912446e8
uuiskk <hus101405@naver.com>
2024-09-24 09:23:10 +0900

    step 2

--- src/main/java/com/nhnacademy/server/method/response/impl/TimeResponse.java
+++ src/main/java/com/nhnacademy/server/method/response/impl/TimeResponse.java
@@ -30,10 +30,12 @@ public class TimeResponse implements Response {
     @Override
     public String execute(String value) {
         //TODO#1 LocalDateTime을 이용해서 현재 시간을 설정하세요.
-        LocalDateTime now = null;
+        LocalDateTime now = LocalDateTime.now();
 
         //TODO#2 value(date format) "" or null 이면 DEFAULT_DATETIME_FORMAT 으로 반환 합니다.
-
+        if(value == null || value.isBlank()){
+            return DEFAULT_DATETIME_FORMAT;
+        }
 
         //TODO#3  value(date format) 의해서 formatting 하는 과정에서 value의 형식이 잘못 되었 다면 DEFAULT_DATETIME_FORMAT 으로 반환 합니다.
 
