231363b1b924897d7871ed70b6b77b730ce3bc46
uuiskk <hus101405@naver.com>
2024-09-26 15:25:32 +0900

    complete

--- src/main/java/com/nhnacademy/server/method/response/impl/WhisperResponse.java
+++ src/main/java/com/nhnacademy/server/method/response/impl/WhisperResponse.java
@@ -20,12 +20,13 @@ import org.apache.commons.lang3.StringUtils;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Objects;
 
 public class WhisperResponse implements Response {
     @Override
     public String getMethod() {
         //TODO#2-1 method = "whisper" 반환 합니다.
-        return "";
+        return "whisper";
     }
 
     @Override
@@ -34,9 +35,13 @@ public class WhisperResponse implements Response {
         //method는 whisper, value는 marco hello <-- 입니다.
 
         //TODO#2-2 로기인 되어있지 않다면 "login required!" 반환 합니다.
-
+        if(!Session.isLogin()) {
+            return "login required!";
+        }
         //TODO#2-3 value null or "" 이면 "empty message!" 반환 합니다.
-
+        if(Objects.isNull(value) || value.isEmpty()) {
+            return "empty message!";
+        }
 
         /*TODO#2-4 value 형식이
             {clientId} {message} 아니라면 "empty message!" 반환 합니다.
@@ -46,12 +51,23 @@ public class WhisperResponse implements Response {
             - marco nice to meet you (O)
 
          */
+        String[] values = value.split(" +", 2);
+        if(value.length() < 2) {
+            return "empty message!";
+        }
 
+        String clientId = values[0];
+        String message = values[1];
 
         //TODO#2-4 value 가 marco hello 라면 marco아이디를 사용하는 cleint에게 hello message를 응답합니다.
+        try {
+            PrintWriter out = new PrintWriter(MessageServer.getClientSocket(clientId).getOutputStream());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
         //TODO#2-5 메시지 전송이 완료되면, "[whisper][marco]" hello 형태로 반환 합니다.
 
-        return "";
+        return "[whisper]->[" + clientId + "]: " + message;
     }
 }
